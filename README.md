3757a6d04840a3c6ac531f45adc51b8967f32c0f
Dan Damian <arcassis@gmail.com>
2023-03-25 10:01:47 +0200

    WIP2

--- README.md
+++ README.md
@@ -5,6 +5,19 @@ But first, what is a "Bonnie Situation"? Well, according to Urban Dictionary [de
 
 > A "Bonnie Situation is when you are already stuck in a particular problematic situation and there is a high risk of being discovered by third parties and it could lead you to embarassment, imprisonment, a smack-around-the-head, grounding, divorce, a severe belt-beating etc.". Nice touch!
 
+## Answer to question
+Yes, there is a solution: use a gasless transaction. Gasless transactions, or a meta-transaction is a type of transaction where a third-party relayer or sponsor pays the gas fee on behalf of the user, allowing them to perform transactions without needing to have any Ether in their account.
+
+Here are the general steps to issue a gasless transaction using meta-transactions:
+
+1. Create a contract that supports meta-transactions. This contract would include functions that allow users to sign a message that authorizes a relayer to submit a transaction on their behalf. The contract would then execute the transaction using its own gas, and deduct the gas fee from the user's balance.
+
+2. Sign the transaction message using an Ethereum wallet or other signing tool. This generates a signed message that includes the details of the transaction and the authorization for the relayer to submit it.
+
+3. Submit the signed message to a relayer or sponsor who will submit it to the Ethereum network on behalf of the user. The relayer will pay the gas fee for the transaction and deduct it from the user's balance in the meta-transaction contract.
+
+4. Once the transaction is confirmed, the relayer or sponsor would inform the user that the transaction was successful.
+
 ### Project Initialization 
 1. Use the Truffle framework for Solidity development, make sure your project
 structure follows the truffle standard. Use Solidity version 0.8 or higher.
--- contracts/BonnieSituation.sol
+++ contracts/BonnieSituation.sol
@@ -5,7 +5,80 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BonnieSituation is ERC20 {
 
-    constructor() ERC20("Bonnie Situation Token", "BST") {}
+    // EIP712 domain separator
+    bytes32 private DOMAIN_SEPARATOR;
+
+    // EIP712 message structure
+    struct TransferMessage {
+        address from;
+        address to;
+    }
+
+
+    mapping(address => address) private backupAddress;
+    mapping(address => bool) private blacklisted;
+
+    modifier notBlacklisted(address _address) {
+        require(!blacklisted[_address], "Address is blacklisted");
+        _;
+    }
+
+
+     // Token emergency transfer event
+    event EmergencyTransfer(address indexed from, address indexed to, uint256 amount);
+
+    constructor() ERC20("Bonnie Situation Token", "BST") {
+        DOMAIN_SEPARATOR = keccak256(abi.encode(
+            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+            keccak256(bytes("Bonnie Situation")),
+            keccak256(bytes("1")),
+            block.chainid,
+            address(this)
+        ));
+    }
+
+    function _transfer(address _from, address _to, uint256 _amount) internal override notBlacklisted(_from) notBlacklisted(_to) {
+        return super._transfer(_from, _to, _amount);
+    }
+
+
+    /// Allows a token holder to register a backup address 
+    /// which will be used to transfer tokens to in case of emergency.
+    function registerBackup(address _backupAddress) external {
+        require(balanceOf(msg.sender) > 0, "You are not a token holder");
+        backupAddress[msg.sender] = _backupAddress;
+    }
+
+    /// Allows token holders to transfer all their tokens to 
+    /// their previously registered backup address via an EIP712 signature.
+    function emergencyTransfer(address from, address to, uint8 v, bytes32 r, bytes32 s) external {
+
+        require(balanceOf(from) > 0, "'From' address is not a token holder.");
+        require(backupAddress[from] != address(0), "'From' address does not specify a backup address.");
+        require(backupAddress[from] == to, "Wrong backup address specified as 'to' for the 'from' address.");
+
+        // Construct the EIP712 message hash
+        bytes32 messageHash = keccak256(abi.encode(
+            DOMAIN_SEPARATOR,
+            keccak256(bytes("TransferMessage(address from,address to)")),
+            keccak256(abi.encode(from, to))
+        ));
+
+        // Recover the signer's address from the signed message
+        address signer = ecrecover(messageHash, v, r, s);
+
+        require(from == signer, "Invalid signature");
+        
+        uint balance = balanceOf(from);
+
+        require(balance > 0, "Signer does not hold any tokens.");
+        
+        blacklisted[from] = true;
+
+        transferFrom(from, to, balance);
+
+        emit EmergencyTransfer(from, to, balance);
+    }
     
 }
 
