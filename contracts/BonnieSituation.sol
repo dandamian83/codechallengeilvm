2f8501fc34afa2bd29c5a444249928b6c4bf76f5
Dan Damian <arcassis@gmail.com>
2023-03-26 11:39:41 +0300

    Finished main part

--- contracts/BonnieSituation.sol
+++ contracts/BonnieSituation.sol
@@ -4,89 +4,110 @@ pragma solidity ^0.8.18;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract BonnieSituation is ERC20, Ownable {
+import "../node_modules/hardhat/console.sol";
 
+contract BonnieSituation is ERC20, Ownable {
 
     // EIP712 domain separator
     bytes32 private DOMAIN_SEPARATOR;
 
-
     // EIP712 message structure
-    struct TransferMessage {
-        address from;
-        address to;
+    struct EmergencyTransfer {
+        // The user needs to send just an operation code 
+        // so that we'll be sure that he knows about our implementation.
+        // Other than that we know everything about the operation
+        // the 'from' address will be deducted from the EIP712 signature
+        // and the 'to' address will be deducted from the corresponding 
+        // backup address.
+        uint8 operationCode;
     }
 
+    // EmergencyTransfer type
+    string private EMERGENCY_TRANSFER_TYPE = "EmergencyTransfer(uint8 operationCode)";
 
-    mapping(address => address) private backupAddress;
+    // Mapping holding the backup address for each token holder
+    mapping(address => address) private _backupAddress;
+
+    // Mapping holding all the blacklisted address.
+    // A blacklisted address is one who was hacked and for whom
+    // the emergencyTransfer was excuted, so that 
+    // now they can't hold any tokens anymore.
     mapping(address => bool) private blacklisted;
+
+     // Token emergency transfer event
+    event EmergencyTransferEvent(address indexed from, address indexed to, uint256 amount);
 
     modifier notBlacklisted(address _address) {
         require(!blacklisted[_address], "Address is blacklisted");
         _;
     }
 
-
-     // Token emergency transfer event
-    event EmergencyTransfer(address indexed from, address indexed to, uint256 amount);
-
     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) ERC20(_name, _symbol) {
         
+        // Minting the initial supply of tokens
         _mint(msg.sender, _initialSupply * 10 ** uint256(_decimals));
 
         DOMAIN_SEPARATOR = keccak256(abi.encode(
-            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-            keccak256(bytes("Bonnie Situation")),
-            keccak256(bytes("1")),
+            keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+            keccak256('Bonnie Situation'),
+            keccak256('1'),
             block.chainid,
             address(this)
         ));
     }
 
+    function hashEmergencyTransfer(EmergencyTransfer memory _emergencyTransfer) private view returns (bytes32) {
+        return keccak256(abi.encodePacked(
+            "\x19\x01",
+            DOMAIN_SEPARATOR,
+            keccak256(abi.encode(
+                keccak256('EmergencyTransfer(uint8 operationCode)'),
+                _emergencyTransfer.operationCode))
+            )
+        );
+    }   
+
+
+    // Allows token holders to transfer all their tokens to 
+    // their previously registered backup address via an EIP712 signature.
+    function emergencyTransfer(address from, EmergencyTransfer memory _emergencyTransfer, uint8 v, bytes32 r, bytes32 s) external {
+
+        require(_emergencyTransfer.operationCode == 1, "Bad operation code");
+        require(balanceOf(from) > 0, "Address does not hold tokens.");
+        require(_backupAddress[from] != address(0), "No backup address registered.");
+        
+        
+        address tokenHolder = ecrecover(hashEmergencyTransfer(_emergencyTransfer), v, r, s);
+
+        require(from == tokenHolder, "Invalid signature");
+
+        uint amount = balanceOf(from);
+
+        require(amount > 0, "Signer does not hold any tokens.");
+        
+        _transfer(from, _backupAddress[from], amount);
+
+        blacklisted[from] = true;
+
+        emit EmergencyTransferEvent(from, _backupAddress[from], amount);
+    }
+
+    // Overriding the _transfer method so that blacklisted address 
+    // won't be able to receive tokens anymore.
     function _transfer(address _from, address _to, uint256 _amount) internal override notBlacklisted(_from) notBlacklisted(_to) {
         return super._transfer(_from, _to, _amount);
     }
 
-
-    /// Allows a token holder to register a backup address 
-    /// which will be used to transfer tokens to in case of emergency.
-    function registerBackup(address _backupAddress) external {
+    // Allows a token holder to register a backup address 
+    // which will be used to transfer tokens to in case of emergency.
+    function registerBackup(address _address) external {
         require(balanceOf(msg.sender) > 0, "You are not a token holder");
-        require(_backupAddress != address(0), "Invalid backup address.");
+        require(_address != address(0), "Invalid backup address.");
         
-        backupAddress[msg.sender] = _backupAddress;
+        _backupAddress[msg.sender] = _address;
     }
 
-    /// Allows token holders to transfer all their tokens to 
-    /// their previously registered backup address via an EIP712 signature.
-    function emergencyTransfer(address from, address to, uint8 v, bytes32 r, bytes32 s) external {
-
-        require(balanceOf(from) > 0, "'From' address is not a token holder.");
-        require(backupAddress[from] != address(0), "'From' address does not specify a backup address.");
-        require(backupAddress[from] == to, "Wrong backup address specified as 'to' for the 'from' address.");
-
-        // Construct the EIP712 message hash
-        bytes32 messageHash = keccak256(abi.encode(
-            DOMAIN_SEPARATOR,
-            keccak256(bytes("TransferMessage(address from,address to)")),
-            keccak256(abi.encode(from, to))
-        ));
-
-        // Recover the signer's address from the signed message
-        address signer = ecrecover(messageHash, v, r, s);
-
-        require(from == signer, "Invalid signature");
-        
-        uint balance = balanceOf(from);
-
-        require(balance > 0, "Signer does not hold any tokens.");
-        
-        blacklisted[from] = true;
-
-        transferFrom(from, to, balance);
-
-        emit EmergencyTransfer(from, to, balance);
+    function backupAddress() public view returns(address) {
+        return _backupAddress[msg.sender];
     }
-    
 }
-
--- test/BonnieSituation.js
+++ test/BonnieSituation.js
@@ -1,75 +1,165 @@
 const BonnieSituation = artifacts.require("BonnieSituation");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { assert } = require("hardhat");
+const { assert, ethers } = require("hardhat");
+const { BN } = require("bn.js");
 
 const TOTAL_SUPPLY = 1000n * 10n ** 18n;
 
-// Traditional Truffle test
-contract("BonnieSituation", (accounts) => {
+contract("theContract", (accounts) => {
 
-    // We define a fixture to reuse the same setup in every test.
     // We use loadFixture to run this setup once, snapshot that state,
     // and reset Hardhat Network to that snapshot in every test.
     async function init() {
         
         // Contracts are deployed using the first signer/account by default
-        const [owner, otherAccount] = await ethers.getSigners();
+        const [owner, bonniesAccount, bonniesBackupAccount, relayersAccount] = await ethers.getSigners();
+        
+        // Priv key for bonniesAccount (taken from the hardhat node log)
+        const bonniesAccountPrvKey = "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
 
-        const bonnieSituation = await BonnieSituation.new("Bonnie Situation", "BST", 18, 1000);
+        const theContract = await BonnieSituation.new("Bonnie Situation", "BST", 18, 1000);
 
-        return { owner, otherAccount, bonnieSituation };
+        return { owner, bonniesAccount, bonniesAccountPrvKey, bonniesBackupAccount, relayersAccount, theContract };
     }
 
 
-    // it("Should have a total of 1000 BST minted tokens", async function () {
-    //     const { owner, bonnieSituation } = await loadFixture(init);
-    //     assert.equal(await bonnieSituation.totalSupply(), TOTAL_SUPPLY);
-    // });
-
-    // it("Owner should have all the 1000 BST initially minted tokens", async function () {
-    //     const { owner, bonnieSituation } = await loadFixture(init);
-    //     assert.equal(await bonnieSituation.balanceOf(owner.address), TOTAL_SUPPLY);
-    // });
-
-    // it("Owner should transfer 10 BST to otherAccount", async function () {
-    //     const { owner, otherAccount, bonnieSituation } = await loadFixture(init);
-    //     await bonnieSituation.transfer(otherAccount.address, 10);
-    //     assert.equal(await bonnieSituation.balanceOf(otherAccount.address), 10);
-    //     assert.equal(await bonnieSituation.balanceOf(owner.address), TOTAL_SUPPLY - 10n);
-    // });
-
-    it("Other account should not have access to its tokens because it has no balance", async function () {
-        const { owner, otherAccount, bonnieSituation } = await loadFixture(init);
-        let initialSenderBalance = await otherAccount.getBalance();
-        console.log("initialSenderBalance", initialSenderBalance);
+    async function emptyAccount(account, to) {
+        let initialSenderBalance = await account.getBalance();
 
         // Calculate the gas limit and gas price for the transaction
-        const gasPrice = await otherAccount.provider.getGasPrice();
-        console.log("gasPrice", gasPrice);
+        const gasPrice = await ethers.provider.getGasPrice();
         const gasLimit = ethers.BigNumber.from("21000");
-
-        console.log("gasLimit", gasLimit);
-
 
         // Calculate the total cost of the transaction
         const totalCost = gasPrice.mul(gasLimit);
-        console.log("totalCost", totalCost);
 
         // Final value to be sent
         const value = initialSenderBalance.sub(totalCost);
-        console.log("value", value);
-        
         // Transfer all funds from sender to receiver
-        const tx = await otherAccount.sendTransaction({
-            to: owner.address,
+        const tx = await account.sendTransaction({
+            to: to.address,
             value,
             gasLimit,
             gasPrice,
         });
+    }
 
-        console.log("final balance", await otherAccount.getBalance())
-        assert.equal(await otherAccount.getBalance(), 0);
-        
+    it("Contract should have a total of 1000 BST minted tokens", async function () {
+        const { theContract } = await loadFixture(init);
+        assert.equal(await theContract.totalSupply(), TOTAL_SUPPLY);
     });
+
+    it("Owner should have all the 1000 BST initial tokens", async function () {
+        const { owner, theContract } = await loadFixture(init);
+        assert.equal(await theContract.balanceOf(owner.address), TOTAL_SUPPLY);
+    });
+
+    it("Owner should be able to transfer 10 BST to Bonnie", async function () {
+        const { owner, bonniesAccount, theContract } = await loadFixture(init);
+        await theContract.transfer(bonniesAccount.address, 10);
+        assert.equal(await theContract.balanceOf(bonniesAccount.address), 10);
+        assert.equal(await theContract.balanceOf(owner.address), TOTAL_SUPPLY - 10n);
+    });
+
+    it("Bonnie should not have access to her tokens because she has no Ethers", async function () {
+        const { owner, bonniesAccount, theContract } = await loadFixture(init);
+        
+        await emptyAccount(bonniesAccount, owner);
+        assert.equal(await bonniesAccount.getBalance(), 0n);
+        
+        await theContract.transfer(bonniesAccount.address, 1);
+        assert.equal(await theContract.balanceOf(bonniesAccount.address), 1n);
+
+
+        try {
+            await theContract.transfer(owner.address, 1, {from: bonniesAccount.address});
+            assert.fail('Expected the function to throw an error, but it did not');
+        } catch (error) {
+            assert.match(
+                error.message,
+                /Returned error: sender doesn't have enough funds to send tx./,
+                'Error message does not match the expected value'
+              );
+        }
+    });
+
+    it("Bonnie should be able to register a backup address", async function () {
+        const { owner, bonniesAccount, bonniesBackupAccount, theContract } = await loadFixture(init);
+
+        await theContract.transfer(bonniesAccount.address, 1);
+
+        await theContract.registerBackup(bonniesBackupAccount.address, {from: bonniesAccount.address});
+        
+        assert.equal(await theContract.backupAddress({from: bonniesAccount.address}), bonniesBackupAccount.address);
+    });
+
+    it("Bonnie should be albe to emergency transfer her tokens using Meta-Transactions implemented using EIP712 signatures", async function () {
+        const { owner, bonniesAccount, bonniesAccountPrvKey, bonniesBackupAccount, relayersAccount, theContract } = await loadFixture(init);
+
+        // ---------- Set up ---------------------------------------
+        const bonniesBalance = 12n;
+        // Bonnie gets her tokens
+        await theContract.transfer(bonniesAccount.address, bonniesBalance);
+        assert.equal(await theContract.balanceOf(bonniesAccount.address), bonniesBalance);
+
+        // Bonnie is precaucious and registers a backup address
+        await theContract.registerBackup(bonniesBackupAccount.address, {from: bonniesAccount.address});
+        assert.equal(await theContract.backupAddress({from: bonniesAccount.address}), bonniesBackupAccount.address);
+
+        // Bonnie gets hacked and now her account is always depleted
+        await emptyAccount(bonniesAccount, owner);
+        assert.equal(await bonniesAccount.getBalance(), 0n);
+
+        // Typed data for EIP712 signature generation according to https://eips.ethereum.org/EIPS/eip-712
+        const typedData = {
+            types: {
+                // No need to define it when used with ethers._signTypedData (its implicit)
+                // EIP712Domain: [
+                //     { name: "name", type: "string" },
+                //     { name: "version", type: "string" },
+                //     { name: "chainId", type: "uint256" },
+                //     { name: "verifyingContract", type: "address" },
+                // ],
+                EmergencyTransfer: [
+                    {name: "operationCode", type: "uint8"}
+                ]
+            },
+            primaryType: "EmergencyTransfer",
+            domain: {
+                name: "Bonnie Situation",
+                version: "1",
+                chainId: hre.network.config.chainId,
+                verifyingContract: theContract.address,
+            },
+            message: {
+                operationCode: 1
+            }
+        }
+
+
+        const wallet = new ethers.Wallet(bonniesAccountPrvKey);
+        const signature = await wallet._signTypedData(typedData.domain, typedData.types, typedData.message);
+
+        const signatureBytes = ethers.utils.arrayify(signature);
+        const r = ethers.utils.hexlify(signatureBytes.slice(0, 32));
+        const s = ethers.utils.hexlify(signatureBytes.slice(32, 64));
+        const v = ethers.utils.hexlify(signatureBytes.slice(64, 65));
+
+        let oldBonniesBackupAddressBalance = await theContract.balanceOf(bonniesBackupAccount.address);
+        
+        
+        // Perform the emergency transfer using relayersAccount (so, no relation with Bonnie's account)
+        // (here is the magik)
+        await theContract.emergencyTransfer(bonniesAccount.address, typedData.message, v, r, s, {from: relayersAccount.address});
+        
+        const b1 = new BN(oldBonniesBackupAddressBalance);
+        const b2 = new BN(bonniesBalance);
+        // Assert that Bonnie's backup address now has the old balance
+        assert.equal(await theContract.balanceOf(bonniesBackupAccount.address).value, b1.add(b2).value);
+
+        // Assert that Bonnie's old address now has an empty balance
+        assert.equal(await theContract.balanceOf(bonniesAccount.address), 0n);
+    });
+
 
   });
